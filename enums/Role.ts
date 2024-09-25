26f770f82624aa4e794236d69aa9fa6f10b3ba5e
Asher Lien <asher.lien@riversoft.com.tw>
2024-09-25 17:13:06 +0800

    更新權限設定

--- enums/Role.ts
+++ enums/Role.ts
@@ -1,14 +1,16 @@
 export enum Role {
   "dev" = "dev", // 開發者
   "admin" = "admin", // 管理者
+  "fullTime" = "fullTime", // 全職同工
   "districtLeader" = "districtLeader", // 區域領導者
   "shepherd" = "shepherd", // 牧者
   "user" = "user" // 使用者
 }
 
 export const RoleList = {
-  "users": [Role.dev, Role.admin, Role.districtLeader, Role.shepherd, Role.user],
-  "shepherds": [Role.dev, Role.admin, Role.districtLeader, Role.shepherd],
+  "admins": [Role.dev, Role.admin],
+  "fullTime": [Role.dev, Role.admin, Role.fullTime],
   "districtLeaders": [Role.dev, Role.admin, Role.districtLeader],
-  "admins": [Role.dev, Role.admin]
+  "shepherds": [Role.dev, Role.admin, Role.districtLeader, Role.shepherd],
+  "users": [Role.dev, Role.admin, Role.districtLeader, Role.shepherd, Role.user],
 }
