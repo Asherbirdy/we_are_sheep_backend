9b6824a108adb2bc8725534277fef2826b21a2a9
Asher Lien <asher.lien@riversoft.com.tw>
2024-09-30 13:39:43 +0800

    更新

--- db/index.ts
+++ db/index.ts
@@ -1,8 +1,16 @@
 import mongoose from 'mongoose'
+import config from '../config'
 mongoose.set('strictQuery', false)
 
 export const connectDB = (url: string, dbName: string) => {
-  const connectionUrl = `${ url }/${ dbName }?retryWrites=true&w=majority`
-  return mongoose.connect(connectionUrl)
+  if(config.environment === 'DEV') {
+    const connectionUrl = `${ url }/${ dbName }?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.0`
+    return mongoose.connect(connectionUrl)
+  }
+
+  if(config.environment === 'PROD') {
+    const connectionUrl = `${ url }/${ dbName }?retryWrites=true&w=majority`
+    return mongoose.connect(connectionUrl)
+  }
 }
 
