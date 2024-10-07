80d49ebb7b5e97ded405b3263e5c8ce2ef714a14
Asher Lien <asher.lien@riversoft.com.tw>
2024-10-07 14:31:32 +0800

    editMember

--- controllers/MemberController.ts
+++ controllers/MemberController.ts
@@ -101,8 +101,16 @@ export const MemberController = {
   // edit member
   editMember: async (req: Req, res: Res) => {
     const { memberId, name, district, identity } = req.body
+    if (!memberId || !name || !district || !identity) {
+      res.status(StatusCodes.BAD_REQUEST).json({
+        msg: 'Please provide memberId, name, district, identity',
+      })
+      return
+    }
+    const member = await Member.findByIdAndUpdate(memberId, { name, district, identity }, { new: true })  
     res.status(StatusCodes.OK).json({
       msg: 'MemberController_EDIT Success',
+      member
     })
   },
 }
