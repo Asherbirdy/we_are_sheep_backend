9be02d74aeb8f8a7325a898e8120ac5cedbd88e6
Asher Lien <asher.lien@riversoft.com.tw>
2024-10-15 14:35:35 +0800

    更新

--- controllers/UserSerialNumberController.ts
+++ controllers/UserSerialNumberController.ts
@@ -1,10 +1,12 @@
 import { StatusCodes } from '../enums'
 import { Req, Res } from '../types'
-
+import { v4 as uuidv4 } from 'uuid'
 export const UserSerialNumberController = {
   create: async (req: Req, res: Res) => {
+  
     res.status(StatusCodes.OK).json({
-      msg: 'User serial number created successfully'
+      msg: 'User serial number created successfully',
+      userSerialNumber: uuidv4()
     })
   }
 }
