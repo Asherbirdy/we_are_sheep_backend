0de9fe4882631151efb226cab2d1e9a77a18661a
Asher Lien <asher.lien@riversoft.com.tw>
2024-10-15 17:15:46 +0800

    註解UserModel的member

--- types/models/IUser.ts
+++ types/models/IUser.ts
@@ -1,13 +1,12 @@
 import { Document } from 'mongoose'
 import { Role } from '../../enums'
-import { IMember } from './IMember'
 
 export interface IUser extends Document {
   name: string
   email: string
   password: string
   role: Role
-  member: IMember
+  // member: IMember
   // eslint-disable-next-line no-unused-vars
   comparePassword(candidatePassword: string): Promise<boolean>;
 }
