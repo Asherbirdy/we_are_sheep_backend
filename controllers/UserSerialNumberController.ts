46f198310db9ea08cd91de9d52ae932ee6e3c100
Asher Lien <asher.lien@riversoft.com.tw>
2024-10-16 14:56:46 +0800

    更新

--- controllers/UserSerialNumberController.ts
+++ controllers/UserSerialNumberController.ts
@@ -1,14 +1,33 @@
-import { StatusCodes } from '../enums'
+import { Role, StatusCodes } from '../enums'
 import { Req, Res } from '../types'
 import { v4 as uuidv4 } from 'uuid'
 import { UserSerialNumber } from '../models/UserSerialNumber'
 
 export const UserSerialNumberController = {
   create: async (req: Req, res: Res) => {
+    const { role, districtId, notes } = req.body
+    if (!role || !districtId || !notes) {
+      res.status(StatusCodes.BAD_REQUEST).json({
+        msg: 'Please provide all required fields'
+      })
+      return
+    }
+
+    if(districtId === Role.dev || districtId === Role.admin) {
+      res.status(StatusCodes.BAD_REQUEST).json({
+        msg: 'District id is invalid'
+      })
+      return
+    }
+
     const userSerialNumber = await UserSerialNumber.create({
       serialNumber: uuidv4(),
-      createByUser: req.user?.userId
+      createByUser: req.user?.userId,
+      role,
+      districtId,
+      notes
     })
+    
     res.status(StatusCodes.OK).json({
       msg: 'User serial number created successfully',
       userSerialNumber
