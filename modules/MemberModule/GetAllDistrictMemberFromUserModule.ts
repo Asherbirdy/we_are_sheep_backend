123e806b101a093c6fae45e6084ac658a710553f
Asher Lien <asher.lien@riversoft.com.tw>
2024-10-18 17:59:07 +0800

    GetAllDistrictMemberFromUserModule

--- modules/MemberModule/GetAllDistrictMemberFromUserModule.ts
+++ modules/MemberModule/GetAllDistrictMemberFromUserModule.ts
@@ -1,17 +1,21 @@
 import { StatusCodes } from '../../enums'
+import { Member } from '../../models/Member'
 import { Req, Res } from '../../types'
 
 export const GetAllDistrictMemberFromUserModule =async (req: Req, res: Res) => {
-  console.log(req.user?.districtId)
-  const { districtId } = req.body
-  if (!districtId) {
+  if (!req.user?.districtId) {
     res.status(StatusCodes.BAD_REQUEST).json({
       msg: 'Please provide districtId',
     })
     return
   }
 
+  const members = await Member.find({
+    district: req.user?.districtId,
+  })
+
   res.status(StatusCodes.OK).json({
     msg: 'MemberController_GET_ALL_DISTRICT_MEMBER Success',
+    members,
   })
 }
