313a47fc1197c58eeba721ed54789497679e7acb
Asher Lien <asher.lien@riversoft.com.tw>
2024-10-23 17:31:34 +0800

    新增註解

--- controllers/UserSerialNumberController.ts
+++ controllers/UserSerialNumberController.ts
@@ -4,6 +4,12 @@ import {
   DeleteSerialNumberController
 } from './UserSerialNumber/index'
 
+/*
+  Dev 可以創造 admin 的 serial number
+  admin 可以創造 districtLeader 的 serial number ,不能創造 admin/dev 的 serial number
+  一般都創建 districtLeader 的 serial number 給別人使用
+*/ 
+
 export const UserSerialNumberController = {
   create: CreateSerialNumberController,
   getAll: GetAllSerialNumberController,
--- enums/Role.ts
+++ enums/Role.ts
@@ -1,7 +1,7 @@
 export enum Role {
   "dev" = "dev", // 開發者
   "admin" = "admin", // 管理者 子開發者
-  "districtLeader" = "districtLeader", // 區域領導者
+  "districtLeader" = "districtLeader", // 區域負責人(一般都創建這帳號)
   "user" = "user" // 使用者
 }
 
