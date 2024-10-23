2829cf2072254c01ed46417db8a7055a30f8c0db
Asherbirdy <122598799+Asherbirdy@users.noreply.github.com>
2024-10-23 14:22:24 +0800

    fix/District需unique(#18)
    Co-authored-by: Asher Lien <asher.lien@riversoft.com.tw>

--- controllers/DistrictController.ts
+++ controllers/DistrictController.ts
@@ -3,6 +3,7 @@ import { RoleList, StatusCodes } from '../enums'
 import { District } from '../models/District'
 import User from '../models/User'
 import { checkPersmission, request } from '../utils'
+
 export const DistrictController = {
   get: async (req: Req, res: Res) => {
     const districts = await District.find({})
@@ -19,7 +20,17 @@ export const DistrictController = {
         msg: 'District name is required'
       })
     }
+
+    const findDistrict = await District.findOne({ name })
+    if (findDistrict) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        error: 'CREATE_DUPLICATE_ERROR',
+        msg: 'District already exists'
+      })
+    }
+
     const district = await District.create({ name })
+
     res.status(StatusCodes.OK).json({
       msg: 'DistrictController_CREATE',
       district
@@ -33,83 +44,63 @@ export const DistrictController = {
         msg: 'District name and districtId is required'
       })
     }
+
+    const findDistrict = await District.findOne({ name: newName })
+    if (findDistrict) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        error: 'EDIT_DUPLICATE_ERROR',
+        msg: 'District name already exists'
+      })
+    }
+
     const district = await District.findByIdAndUpdate(districtId, { name: newName }, { new: true })
+    
     if (!district) {
       return res.status(StatusCodes.NOT_FOUND).json({
         error: 'EDIT_ERROR',
         msg: 'District not found'
       })
     }
+    
     res.status(StatusCodes.OK).json({
       msg: 'DistrictController_EDIT Success',
       district
     })
   },
-  close: async (req: Req, res: Res) => {
-    const { districtId } = req.body
-    if (!districtId) {
-      return res.status(StatusCodes.BAD_REQUEST).json({
-        error: 'DELETE_ERROR',
-        msg: 'District id is required'
-      })
-    }
-    const district = await District.findByIdAndUpdate(
-      districtId,
-      { active: false },
-      { new: true }
-    )
-    res.status(StatusCodes.OK).json({
-      msg: 'DistrictController_DELETE',
-      district
-    })
-  },
-  open: async (req: Req, res: Res) => {
-    const { districtId, } = req.body
-    if (!districtId) {
-      return res.status(StatusCodes.BAD_REQUEST).json({
-        error: 'DELETE_ERROR',
-        msg: 'District id is required'
-      })
-    }
-    const district = await District.findByIdAndUpdate(
-      districtId,
-      { active: true },
-      { new: true }
-    )
-    res.status(StatusCodes.OK).json({
-      msg: 'DistrictController_OPEN',
-      district
-    })  
-  },
-  // updateUserDistrict: async (req: Req, res: Res) => {
-  //   const { districtId, userId } = req.body
-
-  //   requestUtil.checkEmpty(res, [districtId, userId])
-  
-  //   if (!req.user) {
-  //     res.status(StatusCodes.UNAUTHORIZED).json({
-  //       msg: 'User not authenticated'
+  // close: async (req: Req, res: Res) => {
+  //   const { districtId } = req.body
+  //   if (!districtId) {
+  //     return res.status(StatusCodes.BAD_REQUEST).json({
+  //       error: 'DELETE_ERROR',
+  //       msg: 'District id is required'
   //     })
-  //     return
   //   }
-
-  //   const user = await User.findOne({ _id: userId })
-  //   if (!user) {
-  //     res.status(StatusCodes.NOT_FOUND).json({
-  //       msg: 'User not found'
+  //   const district = await District.findByIdAndUpdate(
+  //     districtId,
+  //     { active: false },
+  //     { new: true }
+  //   )
+  //   res.status(StatusCodes.OK).json({
+  //     msg: 'DistrictController_DELETE',
+  //     district
+  //   })
+  // },
+  // open: async (req: Req, res: Res) => {
+  //   const { districtId, } = req.body
+  //   if (!districtId) {
+  //     return res.status(StatusCodes.BAD_REQUEST).json({
+  //       error: 'DELETE_ERROR',
+  //       msg: 'District id is required'
   //     })
-  //     return
   //   }
-  //   const district = await District.findById(districtId)
-  //   if (!district) {
-  //     res.status(StatusCodes.NOT_FOUND).json({
-  //       msg: 'District not found'
-  //     })
-  //     return
-  //   }
-    
-  //   user.district = districtId
-  //   await user.save()
-  //   res.status(StatusCodes.OK).json({ msg: 'Success! User district updated to ' + district.name })
-  // }
+  //   const district = await District.findByIdAndUpdate(
+  //     districtId,
+  //     { active: true },
+  //     { new: true }
+  //   )
+  //   res.status(StatusCodes.OK).json({
+  //     msg: 'DistrictController_OPEN',
+  //     district
+  //   })  
+  // },
 }
