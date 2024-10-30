704044b11bf7e8cfcc2e9275b6b2d910ddd1fdf2
Asher Lien <asher.lien@riversoft.com.tw>
2024-10-30 16:33:26 +0800

    EditMemberInfoController

--- controllers/Member/EditMemberInfoController.ts
+++ controllers/Member/EditMemberInfoController.ts
@@ -3,14 +3,14 @@ import { Member } from '../../models/Member'
 import { Req, Res } from '../../types'
 
 export const EditMemberInfoController = async (req: Req, res: Res) => {
-  const { memberId, name, district, identity } = req.body
-  if (!memberId || !name || !district || !identity) {
+  const { memberId, name, identity } = req.body
+  if (!memberId || !name || !identity) {
     res.status(StatusCodes.BAD_REQUEST).json({
-      msg: 'Please provide memberId, name, district, identity',
+      msg: 'Please provide memberId, name, identity',
     })
     return
   }
-  const member = await Member.findByIdAndUpdate(memberId, { name, district, identity }, { new: true })
+  const member = await Member.findByIdAndUpdate(memberId, { name, district: req.user?.districtId, identity }, { new: true })
   res.status(StatusCodes.OK).json({
     msg: 'MemberController_EDIT Success',
     member
