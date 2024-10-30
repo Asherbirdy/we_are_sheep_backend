73d4bc69e4b0b62a2e5811550633382b15ffe22e
Asher Lien <asher.lien@riversoft.com.tw>
2024-10-30 16:44:38 +0800

    完成

--- controllers/Member/EditMemberMeetingStatusController.ts
+++ controllers/Member/EditMemberMeetingStatusController.ts
@@ -10,12 +10,24 @@ export const EditMemberMeetingStatusController = async (req: Req, res: Res) => {
     })
     return
   }
+  
+  const member = await Member.findOne({ _id: memberId })
+  if (!member) {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      msg: 'Member not found',
+    })
+    return
+  }
 
-  const member = await Member.findByIdAndUpdate(
-    memberId,
-    { meetingStatus },
-    { new: true }
-  )
+  if(member.district.toString() !== req.user?.districtId) {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      msg: 'You are not allowed to edit this member',
+    })
+    return
+  }
+
+  member.meetingStatus = meetingStatus
+  await member.save()
   
   res.status(StatusCodes.OK).json({
     msg: 'MemberController_EDIT_MEMBER_STATUS Success',
