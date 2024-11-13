0742924ffd3199dc1936720a3098111b4d6fc98b
Asher Lien <asher.lien@riversoft.com.tw>
2024-11-13 14:50:52 +0800

    model

---
+++ models/Activity.ts
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from 'mongoose'
+import { IUser } from '../types'
+
+interface IActivity {
+  date: Date
+  year: number
+  month: number
+  title: string
+  content: string
+  createdBy: IUser
+  lastUpdatedBy: IUser
+}
+const ActivitySchema: Schema<IActivity> = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: true
+  },
+  year: {
+    type: Number,
+  },
+  month: {
+    type: Number,
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+  },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  lastUpdatedBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, { timestamps: true })
+
+export const Activity = mongoose.model<IActivity>('Activity', ActivitySchema)
