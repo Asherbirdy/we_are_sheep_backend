e64538a655a4d3a0e557f62c2e6adb85a9f57b6c
Asher Lien <asher.lien@riversoft.com.tw>
2024-11-14 10:54:49 +0800

    更新

--- controllers/User/ShowCurrentUserController.ts
+++ controllers/User/ShowCurrentUserController.ts
@@ -1,8 +1,11 @@
 import { StatusCodes } from '../../enums'
+import User from '../../models/User'
 import { Req, Res } from '../../types'
 
 export const ShowCurrentUserController = async (req: Req, res: Res) => {
+  const user = await User.findById(req.user?.userId).select('-password -OTPAttempts -isBlocked -__v')
   res.status(StatusCodes.OK).json({
-    user: req.user
+    msg: 'User fetched successfully',
+    user
   })
 }
