77f1d0d366750b3db8bf9aea2c492fbdf880c07a
Asher Lien <asher.lien@riversoft.com.tw>
2024-11-15 15:45:32 +0800

    更新

--- controllers/Member/EditDistrictMemberGroupController.ts
+++ controllers/Member/EditDistrictMemberGroupController.ts
@@ -2,8 +2,11 @@ import { StatusCodes } from '../../enums'
 import { Req, Res } from '../../types'
 
 export const EditDistrictMemberGroupController = async (req: Req, res: Res) => {
+  const { memberGroup } = req.body
+  
   res.status(StatusCodes.OK).json({
     msg: 'EditDistrictMemberGroupController',
+    memberGroup
   })
 }
 
