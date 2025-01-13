e6bbef5ecdd0e98914e9953c0d536695fbb2ae06
Asher Lien <birdym.tw@gmail.com>
2025-01-13 12:01:08 +0800

    SheepSchema

---
+++ models/Sheep.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from 'mongoose'
+
+interface ISheep {
+  name: string
+  ageRange: string
+  tags: string[]
+  forcusPerson: boolean
+}
+
+const SheepSchema: Schema<ISheep> = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  ageRange: {
+    type: String,
+    required: true
+  },
+  tags: {
+    type: [String],
+    required: true,
+    validate: {
+      validator: (value: string[]) => value.every((tag) => tag.length <= 5),
+      message: '每個 tag 最多只能有五個字'
+    }
+  },
+  forcusPerson: {
+    type: Boolean,
+    required: true
+  }
+}, { timestamps: true })
+
+export const Sheep = mongoose.model<ISheep>('Sheep', SheepSchema)
