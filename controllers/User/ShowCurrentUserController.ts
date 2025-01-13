dba9a2800adfc8e1e31ff0b4dbd2913cb1570ef7
Asher Lien <birdym.tw@gmail.com>
2025-01-13 11:24:43 +0800

    完成

--- controllers/User/ShowCurrentUserController.ts
+++ controllers/User/ShowCurrentUserController.ts
@@ -3,7 +3,10 @@ import User from '../../models/User'
 import { Req, Res } from '../../types'
 
 export const ShowCurrentUserController = async (req: Req, res: Res) => {
-  const user = await User.findById(req.user?.userId).select('-password -OTPAttempts -isBlocked -__v')
+  const user = await User.findById(req.user?.userId)
+    .select('-password -OTPAttempts -isBlocked -__v')
+    .populate('district')
+
   res.status(StatusCodes.OK).json({
     msg: 'User fetched successfully',
     user
