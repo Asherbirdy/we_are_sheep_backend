2797e9608006ae91a591d6fa45c1cb6d3601601a
Asher Lien <birdym.tw@gmail.com>
2025-01-16 14:07:41 +0800

    完成

--- controllers/Sheep/EditSheepController.ts
+++ controllers/Sheep/EditSheepController.ts
@@ -1,9 +1,41 @@
 import { StatusCodes } from '../../enums'
 import { Sheep } from '../../models/Sheep'
 import { Req, Res } from '../../types'
+import { checkPersmission, getUserIdByString } from '../../utils/checkPersmission'
 
 export const EditSheepController = async (req: Req, res: Res) => {
+  const { sheepId, ageRange, tags, focusPerson ,personStatus, note} = req.body
+
+  if(!sheepId || !ageRange) {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      errorCode: 'SHEEP_ID_OR_AGE_RANGE',
+      msg: 'sheepId 或 ageRange 不能為空'
+    })
+    return
+  }
+
+  const sheep = await Sheep.findById(sheepId)
+  if(!sheep) {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      errorCode: 'SHEEP_NOT_FOUND',
+      msg: '不存在'
+    })
+    return
+  }
+
+  // 檢查權限
+  checkPersmission(req.user, getUserIdByString(sheep))
+
+  // 更新羊
+  sheep.ageRange = ageRange
+  sheep.tags = tags
+  sheep.focusPerson = focusPerson
+  sheep.personStatus = personStatus
+  sheep.note = note
+  await sheep.save()
+
   res.status(StatusCodes.OK).json({
-    msg: 'ok'
+    msg: 'ok',
+    sheep
   })
 }
