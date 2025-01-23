d110d9837db4d1e792f37580d41c0d08c6f47191
Asher Lien <birdym.tw@gmail.com>
2025-01-23 17:20:40 +0800

    wip

--- controllers/Sheep/GetUserDistrictSheepController.ts
+++ controllers/Sheep/GetUserDistrictSheepController.ts
@@ -1,8 +1,18 @@
 import { Req, Res } from '../../types'
 import { StatusCodes } from '../../enums'
+import { Sheep } from '../../models/Sheep'
 
 export const GetUserDistrictSheepController = async (req: Req, res: Res) => {
+  const findSheep = await Sheep.find({
+    district: req.user?.districtId,
+    focusPerson: 'isFocus'
+  }).populate({
+    path: 'userId',
+    select: '_id name'
+  })
+
   res.status(StatusCodes.OK).json({
-    msg: 'GetUserDistrictSheepController'
+    msg: 'GetUserDistrictSheepController',
+    data: findSheep
   })
 }
