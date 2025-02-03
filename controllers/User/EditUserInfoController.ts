28400a5c9147a6506f45308fed8a313ec8cf782c
Asher Lien <birdym.tw@gmail.com>
2025-02-03 15:09:51 +0800

    done

--- controllers/User/EditUserInfoController.ts
+++ controllers/User/EditUserInfoController.ts
@@ -1,6 +1,6 @@
 import { StatusCodes } from '../../enums'
 import { Req, Res } from '../../types'
-
+import User from '../../models/User'
 export const EditUserInfoController = async (req: Req, res: Res) => {
   const { name } = req.body
 
@@ -12,8 +12,9 @@ export const EditUserInfoController = async (req: Req, res: Res) => {
     return
   }
 
+  await User.findByIdAndUpdate(req.user?.userId, { name }, { new: true })
+
   res.status(StatusCodes.OK).json({
     msg: 'EditUserInfoController_PUT_SUCCESS',
-    data: req.body
   }) 
 }
