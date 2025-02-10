4320140e304f62dbffb63b1a765261edad016b46
Asherbirdy <122598799+Asherbirdy@users.noreply.github.com>
2025-02-10 10:36:32 +0800

    Feat/刪掉帳號功能 (#95)
    * wip
    
    * done

--- controllers/Dev/DeleteUserController.ts
+++
@@ -1,9 +0,0 @@
-import { StatusCodes } from '../../enums'
-import { Req, Res } from '../../types'
-
-export const DeleteUserController = async (req: Req, res: Res) => {
-
-  res.status(StatusCodes.OK).json({
-    msg: 'DeleteUserController_DELETE_SUCCESS',
-  })
-}
