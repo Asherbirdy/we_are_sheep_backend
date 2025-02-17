3bf6aca224505610b9d2765c0deda16c76bbef45
Asher Lien <birdym.tw@gmail.com>
2025-02-17 16:31:34 +0800

    done

--- public/C/assets/login-dd606b18.js
+++
@@ -1,9 +0,0 @@
-import{d as i,M as m,N as r,_ as f,c as _}from"./ThemeSwichComponent.vue_vue_type_script_setup_true_lang-20285c94.js";import{d as w,r as h,a0 as y,L as N,M as x,O as o,P as t,Q as e,T as g,$ as l,_ as b,h as C}from"./index-92fc2b15.js";import{N as k}from"./p-371a3378.js";import{N as L}from"./Dropdown-0015ad5a.js";const T=i(!0),I=w({__name:"login",setup(v){const a=h({dropdown:{show:!1}}),c=[{label:"Theme",key:"Theme"},{label:"其他",key:"The其他me"}],d=s=>{switch(s.key){case"Theme":return C(f);default:return s.label}};return(s,n)=>{const p=_,u=y("router-view");return N(),x(e(r),{"content-style":`
-      padding: 24px;
-      margin: auto;
-      max-width: 1200px;`},{default:o(()=>[t(e(l),{justify:"space-between",align:"center"},{default:o(()=>[t(e(k),null,{default:o(()=>n[1]||(n[1]=[g("測試環境")])),_:1}),t(e(l),{align:"center"},{default:o(()=>[t(p),t(e(L),{show:e(a).dropdown.show,options:c,placement:"bottom-start","render-label":d},{default:o(()=>[t(e(b),{size:30,color:"gray",cursor:"pointer",onClick:n[0]||(n[0]=$=>e(a).dropdown.show=!e(a).dropdown.show)},{default:o(()=>[t(e(m),{"mt-2":""})]),_:1})]),_:1},8,["show"])]),_:1})]),_:1}),t(e(r),null,{default:o(()=>[t(e(T),{"content-style":`
-          height: calc(100dvh - 92px);
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          `},{default:o(()=>[t(u)]),_:1})]),_:1})]),_:1})}}});export{I as default};
