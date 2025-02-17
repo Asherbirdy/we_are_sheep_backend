41af967b1e3e0cb7ee0398672283f94e1cf1c1eb
Asher Lien <birdym.tw@gmail.com>
2025-02-17 09:24:32 +0800

    定義定義model

---
+++ models/TeemMeetingAttend.ts
@@ -0,0 +1,74 @@
+import mongoose, { Schema } from 'mongoose'
+
+interface ITeemMeetingAttend {
+  district: string
+  name: string
+  ageRange: string
+  gender: string
+  meetingStatus: string
+  sheepherd: string
+  inviteList: string
+}
+
+const TeemMeetingAttendSchema: Schema<ITeemMeetingAttend> = new Schema({
+  district: {
+    type: String,
+    enum: [
+      '未選擇',
+      '一區',
+      '二區',
+      '三區',
+      '四區',
+    ],
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  ageRange: {
+    type: String,
+    enum: [
+      '未選擇',
+      '青職',
+      '大專',
+      '青少年',
+      '其他',
+      '兒童-幼兒園',
+      '兒童-低年級',
+      '兒童-高年級',
+    ],
+    required: true
+  },
+  gender: {
+    type: String,
+    enum: [
+      '未選擇',
+      '男',
+      '女',
+    ],
+    required: true
+  },
+  meetingStatus: {
+    type: String,
+    enum: [
+      '未選擇',
+      '已有聚會',
+      '久不聚會',
+      '福音朋友',
+    ],
+    required: true
+  },
+  sheepherd: {
+    type: String,
+    required: true
+  },
+  inviteList: {
+    type: String,
+    required: true
+  },
+}, {
+  timestamps: true
+})
+
+export const TeemMeetingAttend = mongoose.model('TeemMeetingAttend', TeemMeetingAttendSchema)
