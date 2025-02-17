9f389337b80f9f30f7fe4f3310a0b434c3e0bbd2
Asher Lien <birdym.tw@gmail.com>
2025-02-17 15:44:25 +0800

    done

---
+++ public/C/assets/p-371a3378.js
@@ -0,0 +1,8 @@
+import{c as v,i as r,d as C,b,u as l,e as a,f as x,h as z,aW as y}from"./index-92fc2b15.js";const T=v("p",`
+ box-sizing: border-box;
+ transition: color .3s var(--n-bezier);
+ margin: var(--n-margin);
+ font-size: var(--n-font-size);
+ line-height: var(--n-line-height);
+ color: var(--n-text-color);
+`,[r("&:first-child","margin-top: 0;"),r("&:last-child","margin-bottom: 0;")]),P=Object.assign(Object.assign({},l.props),{depth:[String,Number]}),$=C({name:"P",props:P,setup(e){const{mergedClsPrefixRef:n,inlineThemeDisabled:t}=b(e),h=l("Typography","-p",T,y,e,n),i=a(()=>{const{depth:o}=e,d=o||"1",{common:{cubicBezierEaseInOut:c},self:{pFontSize:m,pLineHeight:p,pMargin:g,pTextColor:f,[`pTextColor${d}Depth`]:u}}=h.value;return{"--n-bezier":c,"--n-font-size":m,"--n-line-height":p,"--n-margin":g,"--n-text-color":o===void 0?f:u}}),s=t?x("p",a(()=>`${e.depth||""}`),i,e):void 0;return{mergedClsPrefix:n,cssVars:t?void 0:i,themeClass:s==null?void 0:s.themeClass,onRender:s==null?void 0:s.onRender}},render(){var e;return(e=this.onRender)===null||e===void 0||e.call(this),z("p",{class:[`${this.mergedClsPrefix}-p`,this.themeClass],style:this.cssVars},this.$slots)}});export{$ as N};
