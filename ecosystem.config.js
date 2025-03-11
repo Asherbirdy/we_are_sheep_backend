abe66eafbfc101477e894f6bb1eeeda9e9d29443
Asher Lien <birdym.tw@gmail.com>
2025-03-11 15:55:45 +0800

    更新log名稱

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,9 +18,9 @@ module.exports = {
       ignore_watch: ['node_modules', 'logs'], // ignore files change
       max_memory_restart: '1G', // restart if process use more than 1G memory
       merge_logs: true, // if true, stdout and stderr will be merged and sent to pm2 log
-      output: './logs/access.log', // pm2 log file
-      error: './logs/error.log', // pm2 error log file
-      env: { // environment variable
+      output: `./logs/access/access-${ new Date().toISOString().split('T')[ 0 ] }.log`, 
+      error: `./logs/error/error-${ new Date().toISOString().split('T')[ 0 ] }.log`, 
+      env: {
         PORT: 1207,
         NODE_ENV: 'production',
       },
