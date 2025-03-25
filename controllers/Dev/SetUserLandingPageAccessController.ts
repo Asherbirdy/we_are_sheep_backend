464ec5ac24f4b73952257dcc6f942c0e705b7522
Asher Lien <birdym.tw@gmail.com>
2025-03-25 13:55:59 +0800

    SetUserLandingPageAccessController

--- controllers/Dev/SetUserLandingPageAccessController.ts
+++ controllers/Dev/SetUserLandingPageAccessController.ts
@@ -1,9 +1,19 @@
 import { StatusCodes } from '../../enums'
+import { BadRequestError } from '../../errors'
+import User from '../../models/User'
 import { Req, Res } from '../../types'
 
 export const SetUserLandingPageAccessController = async (req: Req, res: Res) => {
+  const { userId, accessBoolean } = req.body
+  if (!userId) {
+    throw new BadRequestError('USER_ID_REQUIRED')
+  }
+  const user = await User.findByIdAndUpdate(userId, {
+    landingPageAccess: accessBoolean
+  }, { new: true })
   res.status(StatusCodes.OK).json({
     msg: 'Set user landing page access',
+    data: user
   })
 }
 
