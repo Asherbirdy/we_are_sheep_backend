213bdd7dcba31f969aed1fdcd8873d9b078a7ee1
Asher Lien <birdym.tw@gmail.com>
2025-03-28 15:28:10 +0800

    done

--- controllers/LandingPage/GetLandingPageInfoByIdController.ts
+++ controllers/LandingPage/GetLandingPageInfoByIdController.ts
@@ -5,6 +5,13 @@ import { Req, Res } from '../../types'
 
 export const GetLandingPageInfoByIdController = async (req: Req, res: Res) => {
   const { landingPageId } = req.query
+
+  if(!landingPageId) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      msg: 'Landing page id is required'
+    })
+  }
+
   const landingPage = await LandingPage.findById(landingPageId)
 
   if (!landingPage) {
