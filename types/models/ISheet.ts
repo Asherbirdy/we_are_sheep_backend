1c4d3638db11fe33552825ab7e221b7a2aa3b3ff
Asher Lien <birdym.tw@gmail.com>
2025-05-02 15:33:46 +0800

    edit model

--- types/models/ISheet.ts
+++ types/models/ISheet.ts
@@ -1,11 +1,7 @@
-import { SheetType } from '../../enums/SheetType'
 import { IUser } from './IUser'
 
 export interface ISheet {
   name: string
-  type: SheetType[]
-  url: string
   api: string
-  isActive: boolean
   createdBy: IUser
 }
