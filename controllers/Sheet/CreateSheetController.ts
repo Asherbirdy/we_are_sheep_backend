0fb1d2b7182896909459d43bbc8eb3dbd0deeccb
Asher Lien <birdym.tw@gmail.com>
2025-05-14 10:05:18 +0800

    wip

--- controllers/Sheet/CreateSheetController.ts
+++ controllers/Sheet/CreateSheetController.ts
@@ -1,6 +1,7 @@
 import { StatusCodes } from '../../enums'
 import { Req, Res } from '../../types'
 import { Sheet } from '../../models/Sheet'
+
 export const CreateSheetController = async (req: Req, res: Res) => {
   const { name, api } = req.body
   if (!name || !api) {
