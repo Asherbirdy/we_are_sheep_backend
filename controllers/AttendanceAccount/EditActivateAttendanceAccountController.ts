1a55e4054f85a6ee84450b2a97ddd58a16109658
Asher Lien <birdym.tw@gmail.com>
2025-05-15 14:52:56 +0800

    更新

--- controllers/AttendanceAccount/EditActivateAttendanceAccountController.ts
+++ controllers/AttendanceAccount/EditActivateAttendanceAccountController.ts
@@ -1,10 +1,39 @@
 import { StatusCodes } from '../../enums'
-// import { BadRequestError } from '../../errors'
-// import { AttendanceAccount } from '../../models/AttendanceAccount'
+import { BadRequestError } from '../../errors'
+import { AttendanceAccount } from '../../models/AttendanceAccount'
 import { Req, Res } from '../../types'
 
 export const EditActivateAttendanceAccountController = async (req: Req, res: Res) => {
+
+  const { 
+    id,
+    name, 
+    serialNumber,
+    serialNumberExpiredDate,
+    lineProfileId,
+    active
+  } = req.body
+  
+  if (!name || !serialNumber || !serialNumberExpiredDate || !lineProfileId) {
+    throw new BadRequestError('Please provide all fields')
+  }
+
+  const attendanceAccount = await AttendanceAccount.findByIdAndUpdate(id, 
+    {
+      name, 
+      serialNumber, 
+      serialNumberExpiredDate, 
+      lineProfileId, 
+      active
+    },
+    {
+      new: true,
+      runValidators: true
+    }
+  )
+  
   res.status(StatusCodes.OK).json({
     msg: 'Edit activate attendance account',
+    data: attendanceAccount
   })
 }
