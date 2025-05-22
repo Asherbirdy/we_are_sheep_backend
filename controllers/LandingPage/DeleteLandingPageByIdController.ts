86cc2624bb1d49969f99b969c59456b19a3a7419
Asher Lien <birdym.tw@gmail.com>
2025-05-22 16:32:47 +0800

    更新

--- controllers/LandingPage/DeleteLandingPageByIdController.ts
+++ controllers/LandingPage/DeleteLandingPageByIdController.ts
@@ -1,7 +1,23 @@
 import { StatusCodes } from '../../enums'
+import { BadRequestError, NotFoundError } from '../../errors'
+import { LandingPage } from '../../models/LandingPage'
 import { Req, Res } from '../../types'
 
 export const DeleteLandingPageByIdController = async (req: Req, res: Res) => {
+  const { landingPageId } = req.query
+
+  if (!landingPageId) {
+    throw new BadRequestError('LANDING_PAGE_ID_REQUIRED')
+  }
+
+  const landingPage = await LandingPage.findById(landingPageId)
+
+  if (!landingPage) {
+    throw new NotFoundError('LANDING_PAGE_NOT_FOUND')
+  }
+
+  await landingPage.deleteOne()
+
   res.status(StatusCodes.OK).json({
     msg: 'Delete Landing Page',
   })
