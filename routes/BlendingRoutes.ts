b96ae09707b0dba375b5c1baa6c62c69b8840140
Asher Lien <birdym.tw@gmail.com>
2025-05-26 16:39:31 +0800

    更新

--- routes/BlendingRoutes.ts
+++ routes/BlendingRoutes.ts
@@ -1,16 +1,21 @@
 import { Router } from 'express'
 import { BlendingController } from '../controllers'
-import { authenticateUser } from '../middleware'
+import { RoleList } from '../enums'
+import { authenticateUser, authorizePermission, checkVerifiedEmail } from '../middleware'
 const router = Router()
 
 router.get(
   '/createFromSheet',
   authenticateUser,
+  checkVerifiedEmail,
+  authorizePermission(... RoleList.districtLeaders),
   BlendingController.create
 )
 router.get(
   '/getAll',
   authenticateUser,
+  checkVerifiedEmail,
+  authorizePermission(... RoleList.users),
   BlendingController.getAll
 )
 
