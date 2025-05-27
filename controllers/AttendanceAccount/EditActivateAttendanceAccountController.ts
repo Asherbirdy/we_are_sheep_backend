f6cad7bf9e03bb2d69f97430ea4845a445ebf3a1
Asherbirdy <122598799+Asherbirdy@users.noreply.github.com>
2025-05-27 14:17:59 +0800

    fix/EditActivateAttendanceAccountController (#137)

--- controllers/AttendanceAccount/EditActivateAttendanceAccountController.ts
+++ controllers/AttendanceAccount/EditActivateAttendanceAccountController.ts
@@ -8,30 +8,25 @@ export const EditActivateAttendanceAccountController = async (req: Req, res: Res
   const { 
     id,
     name, 
-    serialNumber,
-    serialNumberExpiredDate,
     lineProfileId,
     active
   } = req.body
   
-  if (!name || !serialNumber || !serialNumberExpiredDate || !lineProfileId) {
-    throw new BadRequestError('Please provide all fields')
+  if (!name || !lineProfileId) {
+    throw new BadRequestError('NAME_AND_LINE_PROFILE_ID_REQUIRED')
   }
 
-  const attendanceAccount = await AttendanceAccount.findByIdAndUpdate(id, 
-    {
-      name, 
-      serialNumber, 
-      serialNumberExpiredDate, 
-      lineProfileId, 
-      active
-    },
-    {
-      new: true,
-      runValidators: true
-    }
-  )
-  
+  const attendanceAccount = await AttendanceAccount.findById(id)
+
+  if (!attendanceAccount) {
+    throw new BadRequestError('ATTENDANCE_ACCOUNT_NOT_FOUND')
+  }
+
+  attendanceAccount.name = name
+  attendanceAccount.lineProfileId = lineProfileId
+  attendanceAccount.active = active
+  await attendanceAccount.save()
+
   res.status(StatusCodes.OK).json({
     msg: 'Edit activate attendance account',
     data: attendanceAccount
