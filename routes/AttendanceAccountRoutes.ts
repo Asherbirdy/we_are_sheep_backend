26344491738494de61d03ad59b78ab331818ada1
Asher Lien <birdym.tw@gmail.com>
2025-05-27 14:25:15 +0800

    更新

--- routes/AttendanceAccountRoutes.ts
+++ routes/AttendanceAccountRoutes.ts
@@ -7,7 +7,6 @@ const router = Router()
 router.get(
   '/getAll', 
   authenticateUser,
-  authorizePermission(... RoleList.districtLeaders),
   checkVerifiedEmail,
   AttendanceAccountController.getAllAttendanceAccount
 )
