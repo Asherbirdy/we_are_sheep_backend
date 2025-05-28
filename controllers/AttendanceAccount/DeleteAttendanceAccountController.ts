524f13f82d0274cdebe250425c8cacd541e746ba
Asher Lien <birdym.tw@gmail.com>
2025-05-28 10:52:58 +0800

    wip

--- controllers/AttendanceAccount/DeleteAttendanceAccountController.ts
+++ controllers/AttendanceAccount/DeleteAttendanceAccountController.ts
@@ -1,8 +1,20 @@
 import { StatusCodes } from '../../enums'
 import { Req, Res } from '../../types'
 import { AttendanceAccount } from '../../models/AttendanceAccount'
+import { BadRequestError, NotFoundError } from '../../errors'
+
 export const DeleteAttendanceAccountController = async (req: Req, res: Res) => {
   const { attendanceAccountId } = req.query
+
+  if (!attendanceAccountId) {
+    throw new BadRequestError('ATTENDANCE_ACCOUNT_ID_REQUIRED')
+  }
+
+  const existingAttendanceAccount = await AttendanceAccount.findById(attendanceAccountId)
+
+  if (!existingAttendanceAccount) {
+    throw new NotFoundError('ATTENDANCE_ACCOUNT_NOT_FOUND')
+  }
 
   const attendanceAccount = await AttendanceAccount.findByIdAndDelete(attendanceAccountId)
 
