79c9792300b339ac9d944fb69eee36ce823653cf
Asherbirdy <122598799+Asherbirdy@users.noreply.github.com>
2025-05-28 14:04:56 +0800

    Fix/active cannot be deleted (#139)
    * 更新
    
    * wip
    
    * 更新

---
+++ controllers/AttendanceAccount/DeleteAttendanceAccountController.ts
@@ -0,0 +1,30 @@
+import { StatusCodes } from '../../enums'
+import { Req, Res } from '../../types'
+import { AttendanceAccount } from '../../models/AttendanceAccount'
+import { BadRequestError, NotFoundError } from '../../errors'
+
+export const DeleteAttendanceAccountController = async (req: Req, res: Res) => {
+  const { attendanceAccountId } = req.query
+
+  if (!attendanceAccountId) {
+    throw new BadRequestError('ATTENDANCE_ACCOUNT_ID_REQUIRED')
+  }
+
+  const existingAttendanceAccount = await AttendanceAccount.findById(attendanceAccountId)
+
+  // Check if the attendance account is active
+  if (existingAttendanceAccount?.active) {
+    throw new BadRequestError('ACTIVE_CANNOT_BE_DELETED')
+  }
+
+  if (!existingAttendanceAccount) {
+    throw new NotFoundError('ATTENDANCE_ACCOUNT_NOT_FOUND')
+  }
+
+  const attendanceAccount = await AttendanceAccount.findByIdAndDelete(attendanceAccountId)
+
+  res.status(StatusCodes.OK).json({
+    msg: 'Attendance account deleted',
+    data: attendanceAccount
+  })
+}
