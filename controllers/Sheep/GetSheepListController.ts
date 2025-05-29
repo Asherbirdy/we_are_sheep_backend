12af253eb45eda4eea2cd057da915d6b28f7ace0
Asher Lien <birdym.tw@gmail.com>
2025-05-29 16:24:25 +0800

    更新

--- controllers/Sheep/GetSheepListController.ts
+++ controllers/Sheep/GetSheepListController.ts
@@ -1,22 +1,12 @@
-import { FocusPerson, StatusCodes } from '../../enums'
+import { StatusCodes } from '../../enums'
 import { Sheep } from '../../models/Sheep'
 import { Req, Res } from '../../types'
 
 export const GetSheepListController = async (req: Req, res: Res) => {
-  const sheep = await Sheep.find({
-    userId: req.user?.userId
-  })
-
-  const nonFocusPersonList = sheep.filter((sheep) => sheep.focusPerson === FocusPerson.notFocus)
-  const focusPersonList = sheep.filter((sheep) => sheep.focusPerson === FocusPerson.isFocus)
-
-  const list = {
-    focusPersonList: focusPersonList,
-    nonFocusPersonList: nonFocusPersonList
-  }
+  const sheep = await Sheep.find()
 
   res.status(StatusCodes.OK).json({
     msg: 'GetSheepListController_GET',
-    list
+    sheep
   })
 }
