6d6417050a1221e6ef1b96927a33f24e17c8774f
Asher Lien <birdym.tw@gmail.com>
2025-06-17 15:48:45 +0800

    更新

---
+++ models/LineFellowshipRecord.ts
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from 'mongoose'
+import { ILineAccountMember } from '../types'
+import { ILineFellowshipReportId } from './LineFellowshipReportId'
+
+export interface ILineFellowshipRecord {
+  lineAccountMemberId: ILineAccountMember
+  lineFellowshipReportId: ILineFellowshipReportId
+  hasMorningRevival: boolean
+  homeMeetingGroup: IHomeMeetingGroup[]
+}
+
+interface IHomeMeetingGroup {
+  name: string[]
+  info: string
+}
+
+const LineFellowshipRecordSchema: Schema<ILineFellowshipRecord> = new mongoose.Schema({
+  lineAccountMemberId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'LineAccountMember',
+    required: true
+  },
+  lineFellowshipReportId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'LineFellowshipReportId',
+    required: true
+  },
+  hasMorningRevival: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  homeMeetingGroup: {
+    type: [{ name: [String], info: String }],
+    required: true
+  }
+}, { timestamps: true })
+
+export const LineFellowshipRecord = mongoose.model<ILineFellowshipRecord>(
+  'LineFellowshipRecord', LineFellowshipRecordSchema
+)
