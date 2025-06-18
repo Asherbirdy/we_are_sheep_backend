0d7e4d4308283bb980d208eaf6c32ca32c03a204
Asher Lien <birdym.tw@gmail.com>
2025-06-18 10:29:13 +0800

    更新

--- controllers/FellowshipRecord/EditRecordByIdController.ts
+++ controllers/FellowshipRecord/EditRecordByIdController.ts
@@ -1,8 +1,29 @@
 import { StatusCodes } from '../../enums'
+import { BadRequestError } from '../../errors'
+import { FellowshipRecord } from '../../models/FellowshipRecord'
 import { Req, Res } from '../../types'
 
 export const EditRecordByIdController = async (req: Req, res: Res) => {
+  const { fellowshipRecordId, hasMorningRevival, homeMeetingGroup } = req.body
+
+  if(!fellowshipRecordId || !hasMorningRevival || !homeMeetingGroup) {
+    throw new BadRequestError('ALL_FIELDS_ARE_REQUIRED')
+  }
+
+  const fellowshipRecord = await FellowshipRecord.findById(
+    fellowshipRecordId
+  )
+
+  if (!fellowshipRecord) {
+    throw new BadRequestError('FELLOWSHIP_RECORD_NOT_FOUND')
+  }
+
+  fellowshipRecord.hasMorningRevival = hasMorningRevival
+  fellowshipRecord.homeMeetingGroup = homeMeetingGroup
+  await fellowshipRecord.save()
+
   res.status(StatusCodes.OK).json({
-    message: 'EditRecordByIdController',
+    msg: '編輯成功',
+    fellowshipRecord
   })
 } 
